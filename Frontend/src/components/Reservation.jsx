3be2c089c5886bfebf68c047d3bc3cd9610267bf
Arshan Shaikh <shaikharshan66786@gmail.com>
2024-05-09 23:06:49 +0530

    comit

--- Frontend/src/components/Reservation.jsx
+++ Frontend/src/components/Reservation.jsx
@@ -17,7 +17,7 @@ const Reservation = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:4000/api/v1/reservation/send",
+        "http://localhost:5000/api/v1/reservation/send",
         { firstName, lastName, email, phone, time, date },
         {
           headers: {
