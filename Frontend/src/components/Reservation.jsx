67c9d605495af9228d9fc4a56ec72c96b76fe0a5
Arshan Shaikh <shaikharshan66786@gmail.com>
2024-05-10 12:48:15 +0530

    commit url

--- Frontend/src/components/Reservation.jsx
+++ Frontend/src/components/Reservation.jsx
@@ -17,7 +17,7 @@ const Reservation = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:5000/api/v1/reservation/send",
+        "https://restaurant-bckend.onrender.com/api/v1/reservation/send",
         { firstName, lastName, email, phone, time, date },
         {
           headers: {
